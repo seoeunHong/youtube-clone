720ffcbf1a2b864c91ca98d3041c387f8c031dee
Seoeun Hong <sh6348@nyu.edu>
2022-03-09 03:45:06 -0500

    Add logger

--- src/server.js
+++ src/server.js
@@ -1,9 +1,18 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 3000;
 
 const app = express();
+const logger = morgan("dev"); // return middleware
+
+// Use middleware globally; Used for whole urls
+app.use(logger);
+
+app.get("/", (req, res) => {
+  return res.send("This is my / page");
+});
 
 const handleListening = () =>
   console.log(`Server Listening on port http://localhost:${PORT}`);
-app.listen(3000, handleListening);
+app.listen(PORT, handleListening);
