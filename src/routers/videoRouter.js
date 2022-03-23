e205b99ddeaebe97c811d00a62e150b9d04dc315
Seoeun Hong <sh6348@nyu.edu>
2022-03-23 12:01:17 -0400

    More Advanced parameter
    Limit parameter type as number with using regular expression

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -2,11 +2,9 @@ import express from "express";
 import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 const videoRouter = express.Router();
 
-// /upload should be above /:id -> so that express cannot think /upload is not
-// /:id(parameter) that {id : upload}.
-videoRouter.get("/upload", upload);
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/upload", upload); // <- Order does not matter now because of Regular expression
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 export default videoRouter;
