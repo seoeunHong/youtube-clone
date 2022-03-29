ca804ce0b6702d1e47c4daf50d55290ee86fc90d
Seoeun Hong <sh6348@nyu.edu>
2022-03-28 22:02:38 -0400

    Edit Video Edit, Delete, and Search part

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,7 @@
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
-  const videos = await Video.find({});
+  const videos = await Video.find({}).sort({ createdAt: "desc" });
   res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = async (req, res) => {
@@ -21,9 +21,19 @@ export const getEdit = async (req, res) => {
   res.render("edit", { pageTitle: `Editing ${video.title}`, video });
 };
 
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
+  const { title, description, hashtags } = req.body;
+  const video = await Video.exists({ _id: id });
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  await Video.findByIdAndUpdate(id, {
+    title,
+    description,
+    hashtags: Video.formatHashtags(hashtags),
+  });
+
   res.redirect(`/videos/${id}`);
 };
 
@@ -37,7 +47,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title: title,
       description: description,
-      hashtags: hashtags.split(",").map((word) => `#${word}`),
+      hashtags: Video.formatHashtags(hashtags),
     });
     res.redirect("/");
   } catch (error) {
@@ -46,4 +56,23 @@ export const postUpload = async (req, res) => {
       errorMessage: error._message,
     });
   }
+};
+
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
+  res.redirect("/");
+};
+
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(keyword, "i"),
+      },
+    });
+  }
+  res.render("search", { pageTitle: "Search", videos });
 };
