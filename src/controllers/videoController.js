97847ce8ee667de3766669f3495cf51ef1f49b96
Seoeun Hong <sh6348@nyu.edu>
2022-03-29 20:42:27 -0400

    Finish part of login part

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -16,7 +16,7 @@ export const getEdit = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
   if (!video) {
-    return res.render("404", { pageTitle: "Video not found." });
+    return res.status(404).render("404", { pageTitle: "Video not found." });
   }
   res.render("edit", { pageTitle: `Editing ${video.title}`, video });
 };
@@ -26,7 +26,7 @@ export const postEdit = async (req, res) => {
   const { title, description, hashtags } = req.body;
   const video = await Video.exists({ _id: id });
   if (!video) {
-    return res.render("404", { pageTitle: "Video not found." });
+    return res.status(404).render("404", { pageTitle: "Video not found." });
   }
   await Video.findByIdAndUpdate(id, {
     title,
@@ -51,7 +51,7 @@ export const postUpload = async (req, res) => {
     });
     res.redirect("/");
   } catch (error) {
-    res.render("upload", {
+    res.status(400).render("upload", {
       pageTitle: "Upload Video",
       errorMessage: error._message,
     });
--- src/models/User.js
+++ src/models/User.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
@@ -6,6 +7,11 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   name: { type: String, required: true },
   location: String,
+});
+
+userSchema.pre("save", async function () {
+  // "this" here means the user being saved
+  this.password = await bcrypt.hash(this.password, 5);
 });
 
 const User = mongoose.model("User", userSchema);
