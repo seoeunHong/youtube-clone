e4b42d1b03cf3970f7dfc4dad8ae56cb1c18ac70
Seoeun Hong <sh6348@nyu.edu>
2022-04-27 16:18:55 -0400

    Almost Finish Backend Part

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,8 @@
-import User from '../models/User';
 import fetch from 'node-fetch';
 import bcrypt from 'bcrypt';
+
+import User from '../models/User';
+import Video from '../models/Video';
 
 export const getJoin = (req, res) => res.render('join', {pageTitle: 'Join'});
 export const postJoin = async (req, res) => {
@@ -203,4 +205,11 @@ export const postChangePassword = async (req, res) => {
   return res.redirect('/login');
 };
 
-export const see = (req, res) => res.send('See User');
+export const see = async (req, res) => {
+  const {id} = req.params;
+  const user = await User.findById(id).populate('videos');
+  if (!user) {
+    return res.status(404).render('404', {pageTitle: 'User not found'});
+  }
+  return res.render('profile', {pageTitle: user.name, user});
+};
